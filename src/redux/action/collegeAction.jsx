3474ba2d737483e3c7e9ec03a7da168290d3bc1f
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2021-10-30 11:52:26 +0530

    added college sigin page

--- src/redux/action/collegeAction.jsx
+++ src/redux/action/collegeAction.jsx
@@ -1,19 +1,27 @@
-import { collegeSignUp, collegeSignIn, collegeSignOut } from '../../api'
-import axios from 'axios'
+import { collegeSignUp, collegeSignIn, collegeSignOut } from "../../api";
+import axios from "axios";
 
 export const collegeRegister = (college) => async (dispatch) => {
-    dispatch({ type: 'SIGNUP_REQUEST' })
-    return await axios.post(collegeSignUp(), { college })
-        .then(() => {
-            dispatch({ type: 'SIGNUP_SUCCESS' })
-        })
-        .catch((e) => {
-            console.log(e)
-            dispatch({ type: 'SIGNUP_ERROR', payload: e })
-        })
-}
+  dispatch({ type: "SIGNUP_REQUEST" });
+  return await axios
+    .post(collegeSignUp(), { college })
+    .then(() => {
+      dispatch({ type: "SIGNUP_SUCCESS" });
+    })
+    .catch((e) => {
+      console.log(e);
+      dispatch({ type: "SIGNUP_ERROR", payload: e });
+    });
+};
 
-// export const collegeSignIn = (collegeCode, password) => async (dispatch) => {
-//     const signedIn = await axios.post(CollegeSignIn(), { collegeCode, password })
-
-// }
+export const collegeLogin = (collegeCode, password) => async (dispatch) => {
+  dispatch({ type: "LOGIN_REQUEST" });
+  await axios
+    .post(collegeSignIn(), { collegeCode, password })
+    .then((response) => {
+      console.log(response.data.status);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- src/redux/reducer/collegeReducer.jsx
+++ src/redux/reducer/collegeReducer.jsx
@@ -1,47 +1,47 @@
 const initState = {
-    isAuthenticated: false,
+  isAuthenticated: false,
 
-    loginInProgress: false,
-    loginError: null,
+  loginInProgress: false,
+  loginError: null,
 
-    signupInProgress: false,
-    signupError: null,
-    signupSuccess: false,
+  signupInProgress: false,
+  signupError: null,
+  signupSuccess: false,
 
-    logoutInProgress: false,
-    logoutError: null
-}
+  logoutInProgress: false,
+  logoutError: null,
+};
 
 const collegeReducer = (state = initState, { type, payload }) => {
-    switch (type) {
-        case 'AUTH_INFO_SUCCESS':
-            return {
-                ...state,
-                isAuthenticated: payload
-            }
-        case "LOGIN_REQUEST":
-            return { ...state, loginInProgress: true, loginError: null };
-        case "LOGIN_SUCCESS":
-            return { ...state, loginInProgress: false, isAuthenticated: true };
-        case "LOGIN_ERROR":
-            return { ...state, loginInProgress: false, loginError: payload };
+  switch (type) {
+    case "AUTH_INFO_SUCCESS":
+      return {
+        ...state,
+        isAuthenticated: payload,
+      };
+    case "LOGIN_REQUEST":
+      return { ...state, loginInProgress: true, loginError: null };
+    case "LOGIN_SUCCESS":
+      return { ...state, loginInProgress: false, isAuthenticated: true };
+    case "LOGIN_ERROR":
+      return { ...state, loginInProgress: false, loginError: payload };
 
-        case "SIGNUP_REQUEST":
-            return { ...state, signupInProgress: true, signupError: null };
-        case "SIGNUP_SUCCESS":
-            return { ...state, signupInProgress: false, signupSuccess: true };
-        case "SIGNUP_ERROR":
-            return { ...state, signupInProgress: false, signupError: payload };
+    case "SIGNUP_REQUEST":
+      return { ...state, signupInProgress: true, signupError: null };
+    case "SIGNUP_SUCCESS":
+      return { ...state, signupInProgress: false, signupSuccess: true };
+    case "SIGNUP_ERROR":
+      return { ...state, signupInProgress: false, signupError: payload };
 
-        case "LOGOUT_REQUEST":
-            return { ...state, logoutInProgress: true, logoutError: null };
-        case "LOGOUT_SUCCESS":
-            return { ...state, logoutInProgress: false, isAuthenticated: false };
-        case "LOGOUT_ERROR":
-            return { ...state, logoutInProgress: false, logoutError: payload };
-        default:
-            return { ...state }
-    }
-}
+    case "LOGOUT_REQUEST":
+      return { ...state, logoutInProgress: true, logoutError: null };
+    case "LOGOUT_SUCCESS":
+      return { ...state, logoutInProgress: false, isAuthenticated: false };
+    case "LOGOUT_ERROR":
+      return { ...state, logoutInProgress: false, logoutError: payload };
+    default:
+      return { ...state };
+  }
+};
 
-export default collegeReducer
+export default collegeReducer;
