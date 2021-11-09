1bbe8cf032cad648f4d0e0b9a69e2cefc83133b1
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2021-11-09 20:55:49 +0530

    added clg dashboard route, did API integratrion for collegeSignin and collegeSignup

---
+++ src/Pages/College/CollegeDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CollegeDashboard() {
+  return (
+    <div>
+      <h1>College DashBoard</h1>
+    </div>
+  );
+}
---
+++ src/Pages/College/CollegeSignin.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { collegeLogin } from "../../redux/action/collegeAction";
+
+import { TextField, Button } from "@mui/material";
+
+export default function CollegeSignin() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [collegeCode, setCollegeCode] = useState();
+  const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!collegeCode || !password) {
+      setMsg("Please fill the required fields");
+    } else {
+      try {
+        setLoading(true);
+        setMsg("");
+        dispatch(collegeLogin(collegeCode, password))
+          .then(() => {
+            history.push("/college/dashboard");
+          })
+          .catch((err) => {
+            history.push("/college/signin");
+            setMsg("Input fields are invalid");
+          });
+      } catch (err) {
+        setMsg("There was an error while login");
+      }
+    }
+    setLoading(false);
+    setMsg("");
+  };
+
+  return (
+    <div>
+      <h1>CollegeSginin</h1>
+      <h4>{msg}</h4>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="College Code"
+          margin="normal"
+          variant="outlined"
+          onChange={(e) => setCollegeCode(e.target.value)}
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          margin="normal"
+          variant="outlined"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <Button
+        disabled={loading}
+        onClick={handleSubmit}
+        variant="contained"
+        type="submit"
+      >
+        Sign In
+      </Button>
+    </div>
+  );
+}
