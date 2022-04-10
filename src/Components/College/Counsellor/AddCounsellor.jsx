c523ff01da75f3b92f58c987cc8a1666fb26b2fb
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-04-10 23:28:35 +0530

    Add counsellor component added

---
+++ src/Components/College/Counsellor/AddCounsellor.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./AddCounsellor.module.scss";
+
+//MUI
+import { TextField } from "@mui/material";
+
+export default function AddCounsellor() {
+  return (
+    <>
+      <div className={styles.parent}>
+        <div className="my-2 w-50">
+          <TextField
+            id="outlined-basic"
+            fullWidth
+            label="Name"
+            variant="outlined"
+          />
+        </div>
+        <div className="my-4 w-50">
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            fullWidth
+            variant="outlined"
+            type="password"
+          />
+        </div>
+        <br />
+        <button className="btn btn-success w-50 btn-lg">Add Counsellor</button>
+      </div>
+    </>
+  );
+}
