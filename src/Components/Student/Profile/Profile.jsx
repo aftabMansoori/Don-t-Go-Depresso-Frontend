273e34510ea463323e3b2db4919e4613f0e69a2a
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-04-21 22:18:48 +0530

    Updated the profile UI of student

--- src/Components/Student/Profile/Profile.jsx
+++ src/Components/Student/Profile/Profile.jsx
@@ -36,50 +36,53 @@ export default function Profile() {
           />
         </div>
         <div className={styles.parent + " container"}>
-          {/* <h3>Profile</h3> */}
-          <div className="row d-flex align-items-end">
-            <div className="col-md-4">
-              <div>
-                <img
-                  src="https://i.pinimg.com/236x/07/33/ba/0733ba760b29378474dea0fdbcb97107.jpg"
-                  alt=""
-                  className="img-fluid rounded"
-                />
-              </div>
-            </div>
-            <div className="col-md-8">
-              <div>
-                <label>Name:</label>
-                <p>{user.studentName}</p>
-              </div>
-              <div className="d-flex justify-content-between">
-                <span>
-                  <label>Gender:</label>
-                  <p>{user.gender}</p>
-                </span>
-                <span>
-                  <label>Age:</label>
-                  <p>{user.studentAge}</p>
-                </span>
-                <span>
-                  <label>Class:</label>
-                  <p>{user.studentClass}</p>
-                </span>
-              </div>
-            </div>
+          <div className={styles.space}></div>
+          <div
+            className="w-100 d-flex align-items-center justify-content-center"
+            style={{ marginTop: "-80px" }}
+          >
+            <img
+              src="https://i.pinimg.com/236x/07/33/ba/0733ba760b29378474dea0fdbcb97107.jpg"
+              alt=""
+              className="img-fluid rounded"
+            />
           </div>
-          <div className="my-3">
+          <div
+            className="my-5 py-2 px-3"
+            style={{
+              border: "1px solid rgb(200,200,200)",
+              borderRadius: "10px",
+            }}
+          >
             <div>
-              <label>About Me:</label>
+              <label className="fw-bold">Name:</label>
+              <p>{user.studentName}</p>
+            </div>
+            <div className="d-flex justify-content-between">
+              <span>
+                <label className="fw-bold">Gender:</label>
+                <p>{user.gender}</p>
+              </span>
+              <span>
+                <label className="fw-bold">Age:</label>
+                <p>{user.studentAge}</p>
+              </span>
+              <span>
+                <label className="fw-bold">Class:</label>
+                <p>{user.studentClass}</p>
+              </span>
+            </div>
+            <div>
+              <label className="fw-bold">About Me:</label>
               <p>{user.aboutStudent}</p>
             </div>
-            <label>Email:</label>
+            <label className="fw-bold">Email:</label>
             <p>{user.studentClgEmail}</p>
-            <label>PRN No:</label>
+            <label className="fw-bold">PRN No:</label>
             <p>{user.studentPRN}</p>
-            <label>Phone No:</label>
+            <label className="fw-bold">Phone No:</label>
             <p>{user.studentPhoneNo}</p>
-            <label>Medication: </label>
+            <label className="fw-bold">Medication: </label>
             <p>{user.abtMedication}</p>
           </div>
         </div>
